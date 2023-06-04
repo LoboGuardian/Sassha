538d32ffbbc80018e0f68f4f90ad8ab50137ee16
LoboGuardian <30099451+LoboGuardian@users.noreply.github.com>
2023-06-03 23:26:45 -0400

    Idea 💡

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# Sassha
-
-A SSH Client
-
+#!/usr/bin/env python
+"""
 Sassha is a useful client & server for SSH protocol
 
 I hope to make a Gnome app, Windows App, and Android App, maybe include MacOs and IOS
+
+"""
